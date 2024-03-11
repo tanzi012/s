301e0a75a26060c98f22fe5188592fc53897fd87
tanzi012 <138954670+tanzi012@users.noreply.github.com>
2024-03-11 23:14:13 +0530

    Update prac2.bat

--- prac2.bat
+++ prac2.bat
@@ -7,23 +7,28 @@ login
 exit
 enable secret enpa55
 
-ospf on all routers
-router ospf 1,2
+Configure OSPF on routers
+router ospf 1
 network 192.168.1.0 0.0.0.255 area 0
+router ospf 1
 network 10.1.1.0 0.0.0.3 area 0
 
 ospf authenticate sabhi pc pe
 pass : enpa55
 on all right same router ospf 1
+router ospf 1
 area 0 authentication message_digest
 exit
-interface s0/1/0
+int gig0/0
 ip ospf message-digest-key 1 md5 MDpa55
  
  exit
  exit
  show ip ospf interface
-
+PC0 > ping 192.168.1.3
+Successful
+PC1 > ping 192.168.1.2
+Successful
 
  r1
 
@@ -34,13 +39,18 @@ line console 0
 aaa login authentication default
 end 
 
+r1
 config t
 ip domain name ccnasecurity.com
 crypto key generate rsa
-aaa authentication default SSH-LOGIN 
+aaa authentication default SSH-LOGIN local
 line vty 0 4
 login authentication SSH-LOGIN 
 transport input ssh
+end
 
-cp 
+cp 0
 ssh -l Admin 192.168.1.1
+
+cp1
+ssh 
